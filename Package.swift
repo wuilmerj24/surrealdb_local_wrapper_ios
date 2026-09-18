18a6d93b83e931930fd262466f54b52e1fe48f1e
MariaC15 <mariacastillop15@gmail.com>
2026-09-18 18:57:30 -0500

    FIX

--- Package.swift
+++ Package.swift
@@ -1,35 +1,26 @@
 // swift-tools-version: 6.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "surrealdb_local_wrapper_ios",
+
     platforms: [
         .iOS(.v17)
     ],
+
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "surrealdb_local_wrapper_ios",
             targets: ["surrealdb_local_wrapper_ios"]
-        ),
+        )
     ],
+
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "surrealdb_local_wrapper_ios", path: "Sources/surrealdb_local_wrapper_ios/surrealdb_local_wrapper.xcframework"),
-        .target(
+        .binaryTarget(
             name: "surrealdb_local_wrapper_ios",
-            dependencies: ["surrealdb_local_wrapper_ios"],
-            path: "Sources/surrealdb_local_wrapper_ios",
-            sources: ["surrealdb_local_wrapper.swift"],
-            publicHeadersPath: "include",
-        ),
-        .testTarget(
-            name: "surrealdb_local_wrapper_iosTests",
-            dependencies: ["surrealdb_local_wrapper_ios"]
-        ),
-    ],
-    swiftLanguageModes: [.v6]
+            url: "https://github.com/wuilmerj24/surrealdb_local_wrapper_ios/releases/download/1.0.0/surrealdb_local_wrapper.xcframework.zip",
+            checksum: "61b99672e2c11d94707b33fa6e3e621dcbdccd10e98bf9570b2e0e79075e10b9"
+        )
+    ]
 )
