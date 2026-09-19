e4d06641e25ce2a689ab148afa52ee93becb4e9d
MariaC15 <mariacastillop15@gmail.com>
2026-09-18 19:39:32 -0500

    FIX

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "surrealdb_local_wrapper_ios",
-            url: "https://github.com/wuilmerj24/surrealdb_local_wrapper_ios/releases/download/1.0.1/surrealdb_local_wrapper.xcframework.zip",
+            url: "https://github.com/wuilmerj24/surrealdb_local_wrapper_ios/releases/download/1.0.2/surrealdb_local_wrapper.xcframework.zip",
             checksum: "0a01eb0049bf752f4cb6ebbea325a820d07375d924434bc909148c79bfc7cc5b"
         )
     ]
